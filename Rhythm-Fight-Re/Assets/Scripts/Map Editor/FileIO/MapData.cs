154f1dc771c26b58c3327d45e3105fc472c00dc2
LeeSiu <167082095+leesiuuuu@users.noreply.github.com>
2025-05-03 23:52:55 +0900

    FMOD 삭제

---
+++ Rhythm-Fight-Re/Assets/Scripts/Map Editor/FileIO/MapData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData
+{
+	public string title;
+
+	public string artistName;
+	public string designerName;
+
+	public 
+}
